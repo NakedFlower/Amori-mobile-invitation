7e9eba4ffe95e366f58f31552bac8a20a8ee1ce9
NakedFlower <kokh1206@naver.com>
2025-10-18 19:29:25 +0900

    feat:백엔드 예시화면 Hello, Health 엔드 포인트

---
+++ src/main/java/com/amori/AmoriApplication.java
@@ -0,0 +1,11 @@
+package com.amori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoriApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmoriApplication.class, args);
+    }
+}
